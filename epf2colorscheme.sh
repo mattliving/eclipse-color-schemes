217bbfe819b2ceeb1c3e53e1407074c164bd8b44
Tobias Hoffmann <th.geist@googlemail.com>
2010-09-20 22:06:51 -0700

    Removed leftovers from documentation

--- epf2colorscheme.sh
+++ epf2colorscheme.sh
@@ -1,4 +1,3 @@
-epf-to-colorscheme.sh
 #!/bin/bash
 
 file=$1
@@ -7,4 +6,3 @@ theme=${file%.epf}_colors.epf
 echo "file_export_version=3.0" > $theme
 egrep "SystemDefault" < $file >> $theme
 egrep '[0-9]+,[0-9]+,[0-9]+' < $file >> $theme
-
